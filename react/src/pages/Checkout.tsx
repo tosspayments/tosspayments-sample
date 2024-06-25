959988e74b3816e2367ee88ec77e8dbe52ae5611
moresoo <sshn611@naver.com>
2024-06-25 14:10:14 +0900

    feat: SDK 로드를 useEffect로 수행

--- react/src/pages/Checkout.tsx
+++ react/src/pages/Checkout.tsx
@@ -1,23 +1,16 @@
 import { useEffect, useState } from "react";
 import "../App.css";
 
-const selector = "#payment-method";
-
 // TODO: clientKey는 개발자센터의 결제위젯 연동 키 > 클라이언트 키로 바꾸세요.
 // TODO: 구매자의 고유 아이디를 불러와서 customerKey로 설정하세요. 이메일・전화번호와 같이 유추가 가능한 값은 안전하지 않습니다.
 // @docs https://docs.tosspayments.com/reference/widget-sdk#sdk-설치-및-초기화
 const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
 const customerKey = generateRandomString();
-const tossPayments = TossPayments(clientKey);
 
 export function CheckoutPage() {
   const [ready, setReady] = useState(false);
-  // 회원 결제
-  const [widgets] = useState(tossPayments.widgets({
-    customerKey,
-  }));
-  // 비회원 결제
-  // const widgets = tossPayments.widgets({customerKey: TossPayments.ANONYMOUS});
+  // FIXME: 타입 추가
+  const [widgets, setWidgets] = useState<any>(null);
 
   const amount = {
     currency: 'KRW',
@@ -25,7 +18,30 @@ export function CheckoutPage() {
   }
 
   useEffect(() => {
-    async function initialize() {
+    async function fetchPaymentWidgets() {
+      try {
+        const tossPayments = TossPayments(clientKey);
+
+        // FIXME: loadPayment 메소드로 변경 필요
+        // 회원 결제
+        const widgets = tossPayments.widgets({
+          customerKey,
+        })
+        // 비회원 결제
+        // const widgets = tossPayments.widgets({customerKey: TossPayments.ANONYMOUS});
+
+        // const loadedWidgets = await loadPaymentWidgets(clientKey, customerKey);
+        setWidgets(widgets);
+      } catch (error) {
+        console.error("Error fetching payment widget:", error);
+      }
+    };
+
+    fetchPaymentWidgets();
+  }, [clientKey, customerKey]);
+
+  useEffect(() => {
+    async function renderPaymentWidgets() {
       if (widgets == null) {
         return;
       }
@@ -35,23 +51,24 @@ export function CheckoutPage() {
       // TODO: renderPaymentMethods, renderAgreement, requestPayment 보다 반드시 선행되어야 합니다.
       await widgets.setAmount(amount);
 
-       // ------  결제 UI 렌더링 ------
-       // @docs https://docs.tosspayments.com/reference/widget-sdk#renderpaymentmethods선택자-결제-금액-옵션
-       await widgets.renderPaymentMethods({
-           selector,
-           // 렌더링하고 싶은 결제 UI의 variantKey
-           // 결제 수단 및 스타일이 다른 멀티 UI를 직접 만들고 싶다면 계약이 필요해요.
-           // @docs https://docs.tosspayments.com/guides/payment-widget/admin#멀티-결제-ui
-           variantKey: "DEFAULT"
-       });
+      // ------  결제 UI 렌더링 ------
+      // @docs https://docs.tosspayments.com/reference/widget-sdk#renderpaymentmethods선택자-결제-금액-옵션
+      await widgets.renderPaymentMethods({
+        selector: '#payment-method',
+        // 렌더링하고 싶은 결제 UI의 variantKey
+        // 결제 수단 및 스타일이 다른 멀티 UI를 직접 만들고 싶다면 계약이 필요해요.
+        // @docs https://docs.tosspayments.com/guides/payment-widget/admin#멀티-결제-ui
+        variantKey: "DEFAULT"
+      });
 
-        // ------  이용약관 UI 렌더링 ------
-        // @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자-옵션
-        await widgets.renderAgreement({selector: "#agreement", variantKey: "AGREEMENT"});
+      // ------  이용약관 UI 렌더링 ------
+      // @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자-옵션
+      await widgets.renderAgreement({ selector: "#agreement", variantKey: "AGREEMENT" });
 
-        setReady(true);
+      setReady(true);
     };
-    initialize();
+
+    renderPaymentWidgets();
   }, [widgets]);
 
   return (
@@ -76,10 +93,10 @@ export function CheckoutPage() {
                 onChange={async (event) => {
                   if (event.target.checked) {
                     await widgets.setAmount({
-                        currency: amount.currency,
-                        value: amount.value - 5000,
+                      currency: amount.currency,
+                      value: amount.value - 5000,
                     });
-      
+
                     return;
                   }
       
@@ -102,7 +119,6 @@ export function CheckoutPage() {
             try {
               // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
               // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
-              console.log('window.location.origin + "/success', window.location.origin);
               await widgets.requestPayment({
                 orderId: generateRandomString(),
                 orderName: "토스 티셔츠 외 2건",
@@ -124,17 +140,6 @@ export function CheckoutPage() {
     </div>
   );
 }
-
-// function usePaymentWidget(clientKey: string, customerKey: string) {
-//   return useQuery({
-//     queryKey: ["payment-widget", clientKey, customerKey],
-//     queryFn: () => {
-//       // ------  결제위젯 초기화 ------
-//       // @docs https://docs.tosspayments.com/reference/widget-sdk#sdk-설치-및-초기화
-//       return loadPaymentWidget(clientKey, customerKey);
-//     },
-//   });
-// }
 
 function generateRandomString() {
   return window.btoa(Math.random().toString()).slice(0, 20);
