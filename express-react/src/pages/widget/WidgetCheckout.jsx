057bd05ff2ccb5eb82cce22c50b6cdaff4893774
sybock <sybock@snu.ac.kr>
2024-09-02 14:40:29 +0900

    fix: React 쿠폰 금액 변경 고침

--- express-react/src/pages/widget/WidgetCheckout.jsx
+++ express-react/src/pages/widget/WidgetCheckout.jsx
@@ -85,11 +85,9 @@ export function WidgetCheckoutPage() {
         {/* 이용약관 UI */}
         <div id="agreement" />
         {/* 쿠폰 체크박스 */}
-        <div style={{ paddingLeft: "24px" }}>
+        <div style={{ paddingLeft: "30px" }}>
           <div className="checkable typography--p">
-            <label
-              htmlFor="coupon-box"
-              className="checkable__label typography--regular">
+            <label htmlFor="coupon-box" className="checkable__label typography--regular">
               <input
                 id="coupon-box"
                 className="checkable__input"
@@ -108,7 +106,10 @@ export function WidgetCheckoutPage() {
                     return;
                   }
 
-                  await widgets.setAmount(amount);
+                  await widgets.setAmount({
+                    currency: amount.currency,
+                    value: amount.value,
+                  });
                 }}
               />
               <span className="checkable__label-text">5,000원 쿠폰 적용</span>
@@ -140,7 +141,8 @@ export function WidgetCheckoutPage() {
               // 에러 처리하기
               console.error(error);
             }
-          }}>
+          }}
+        >
           결제하기
         </button>
       </div>
@@ -150,13 +152,15 @@ export function WidgetCheckoutPage() {
           padding: "40px 30px 50px 30px",
           marginTop: "30px",
           marginBottom: "50px",
-        }}>
+        }}
+      >
         <button
           className="button"
           style={{ marginTop: "30px" }}
           onClick={() => {
             navigate("/brandpay/checkout");
-          }}>
+          }}
+        >
           위젯 없이 브랜드페이만 연동하기
         </button>
         <button
@@ -164,7 +168,8 @@ export function WidgetCheckoutPage() {
           style={{ marginTop: "30px" }}
           onClick={() => {
             navigate("/payment/checkout");
-          }}>
+          }}
+        >
           위젯 없이 결제창만 연동하기
         </button>
       </div>
